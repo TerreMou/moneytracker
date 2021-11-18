a0c60941573fe6d2a90918af984586f4b6804dc2
terremou <terrence_mou@foxmail.com>
2021-11-18 15:02:37 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Records from '@/views/Records.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import Myself from '@/views/Myself.vue';
+import NotFound from '@/views/NotFound.vue';
 
 
 Vue.use(VueRouter);
@@ -32,7 +33,11 @@ const routes: Array<RouteConfig> = [
     path: '/myself',
     name: 'Myself',
     component: Myself
-  }
+  },
+  {
+    path: '*', // 路由是从上往下依次检查匹配的，所以这里的 * 表示除上面路径外的其他所有路径。
+    component: NotFound
+  },
 ];
 
 const router = new VueRouter({
