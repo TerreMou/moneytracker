5498eb34e5a906f91676018ddda57e13ac0572a3
terremou <terrence_mou@foxmail.com>
2021-11-20 01:17:10 +0800

    写 Records.vue 页面的前四个部分

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,74 @@
 <template>
-  <div class="nav">
-    <router-link to="/labels">明细</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-    |
-    <router-link to="/myself">我的</router-link>
+  <div class="navLarge">
+      <router-link to="/records" class="item2">
+        <Icon name="record"/>
+      </router-link>
+    <nav>
+      <router-link to="/labels" class="item" active-class="selected">
+        <Icon name="labels"/>
+        明细
+      </router-link>
+      <router-link to="/statistics" class="item" active-class="selected">
+        <Icon name="statistics"/>
+        统计
+      </router-link>
+      <router-link to="/myself" class="item" active-class="selected">
+        <Icon name="myself"/>
+        我的
+      </router-link>
+    </nav>
   </div>
 </template>
 
 <script lang="ts">
+
 export default {
   name: 'Nav'
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav {
-    border: 1px solid red;
+@import "../assets/style/helper.scss";
+
+.navLarge {
+  display: flex;
+  flex-direction: column;
+  > .item2 {
+    .icon {
+      width: 50px;
+      height: 50px;
+      margin-right: 16px;
+      margin-bottom: 14px;
+      color: $color-highlight;
+    }
   }
+}
+.item2:first-child {
+  align-self: flex-end;
+}
+
+nav {
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+
+  > .item.selected {
+    color: $color-highlight;
+  }
+}
 </style>
--- src/views/Records.vue
+++ src/views/Records.vue
@@ -1,13 +1,186 @@
 <template>
-  <div>Records.vue</div>
+  <div>
+
+    <header>
+      <div class="title">
+        <div class="backspace" @click="goBack">
+          <Icon name="goBack"/>
+        </div>
+        <div class="pageTitle">记账</div>
+      </div>
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+    </header>
+
+    <ul class="tags">
+      <li>
+        <Icon name="food"/>
+        食物
+      </li>
+      <li>
+        <Icon name="shopping"/>
+        购物
+      </li>
+      <li>
+        <Icon name="bus"/>
+        出行
+      </li>
+      <li>
+        <Icon name="others"/>
+        其他
+      </li>
+      <li>
+        <button class="add">
+          <Icon name="add"/>
+          新增
+        </button>
+      </li>
+    </ul>
+
+    <div>
+      <label class="comments">
+        <input type="text" placeholder="备注">
+      </label>
+    </div>
+
+    <div>
+      <div>金额显示栏</div>
+    </div>
+
+    <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>+*#</button>
+      <button>0</button>
+      <button>delete</button>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'Record'
+  name: 'Record',
+  methods: {
+    // 后退到上一个页面
+    goBack() {
+      this.$router.go(-1);
+      console.log(this);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/style/helper.scss";
+
+header {
+  padding-top: 2px;
+}
+
+.title {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  > .backspace {
+    > .icon {
+      height: 28px;
+      width: 28px;
+    }
+  }
+
+  > .pageTitle {
+    position: absolute;
+    width: 60px;
+    top: 0;
+    left: 50%;
+    margin-left: -30px;
+    font-size: 1.2em;
+    text-align: center;
+  }
+}
+
+.types {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 10px 0;
+
+  > li {
+    border: 1px solid #020202;
+    border-radius: 3px;
+    width: 20%;
+
+    &.selected {
+      background: $color-highlight;
+    }
+  }
+}
+
+.tags {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 16px 4px;
+
+  > li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 12px;
+    width: 50px;
+    height: 50px;
+
+    > .icon {
+      width: 28px;
+      height: 28px;
+    }
+  }
+
+  .add {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+    background: transparent;
+    border: none;
+
+    > .icon {
+      width: 28px;
+      height: 28px;
+    }
+  }
+}
+
+.comments {
+  display: flex;
+  font-size: 14px;
+
+  > input {
+    border: none;
+    height: 40px;
+    flex-grow: 1;
+    padding-left: 16px;
+    background: #f8f9f9;
+  }
+}
+
 
 </style>
