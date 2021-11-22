230b65ab741d070780736de720722656e0b530e9
terremou <terrence_mou@foxmail.com>
2021-11-22 11:49:04 +0800

    使用 JS 实现组件 Header.vue

--- src/components/Records/Header.vue
+++ src/components/Records/Header.vue
@@ -8,22 +8,36 @@
     </div>
 
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'"
+          @click="selectType('-')">支出
+      </li>
+      <li :class="type === '+' && 'selected'"
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </header>
 
 </template>
 
-<script lang="ts">
+<script lang="js">
 export default {
   name: 'Header',
+  data(){
+    return {
+      type: '-' // ‘-’表示支出，‘+’表示收入
+    };
+  },
   methods: {
-    // 后退到上一个页面
     goBack() {
       this.$router.go(-1);
       console.log(this);
-    }
+    },
+    selectType(type) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('type is unknown');
+      }
+      this.type = type;
+    },
   }
 };
 </script>
