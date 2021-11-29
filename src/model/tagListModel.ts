b9a7dc8b9a2a05872396079a5628a37c8c67973c
terremou <terrence_mou@foxmail.com>
2021-11-29 12:53:00 +0800

    修改tag的名字

--- src/model/tagListModel.ts
+++ src/model/tagListModel.ts
@@ -1,4 +1,4 @@
-const localStorageKeyName = 'tagListModel';
+const localStorageKeyName = 'tagList';
 
 type Tag = {
   id: string;
@@ -8,6 +8,7 @@ type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated'; // 联合类型
+  update: (id:string, name: string) => 'success' | 'duplicated' | 'not_found'
   save: () => void
 }
 
@@ -20,10 +21,26 @@ const tagListModel: TagListModel = {
   },
   create(name) {
     const names = this.data.map(item => item.name);
-    if(names.indexOf(name) >= 0) { return 'duplicated'}
+    if (names.indexOf(name) >= 0) { return 'duplicated';}
     this.data.push({id: name, name: name});
     this.save();
     return 'success';
+  },
+  update(id: string, name: string) {
+    const idList = this.data.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.data.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return 'success';
+      }
+    } else {
+      return 'not_found';
+    }
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -2,7 +2,9 @@
   <Layout>
     <nav-bar>编辑标签</nav-bar>
     <div class="formItem-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="updateTag"
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -34,7 +36,11 @@ export default class EditLabel extends Vue {
     } else {
       this.$router.replace('/404');
     }
-
+  }
+  updateTag(name: string):void {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name)
+    }
   }
 }
 </script>
