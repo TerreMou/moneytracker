5cad7152d35096fea4f8a407c3131a86282a7ea1
terremou <terrence_mou@foxmail.com>
2021-12-06 13:32:32 +0800

    完成列表的 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,31 +2,23 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type: {{ type }}
-      <br/>
-      interval: {{ interval }}
-    </div>
+
+    <ol class="wrapper">
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="comments">{{ item.comments }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
+
   </Layout>
 </template>
-
-<style scoped lang="scss">
-@import "../assets/style/helper.scss";
-
-::v-deep li.type-tabs-item {
-  background: white;
-  height: 52px;
-
-  &.selected {
-    background: $color-highlight;
-    color: white;
-
-    &::after {
-      display: none;
-    }
-  }
-}
-</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -39,9 +31,85 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList(): RecordItem[] {
+    return this.$store.state.recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+
+    type Items = RecordItem[]
+    type HashTableValue = { title: string, items: Items }
+
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdTime!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
   recordTypeList = recordTypeList;
 }
 </script>
+
+<style scoped lang="scss">
+@import "../assets/style/helper.scss";
+
+%item {
+  padding: 8px 16px;
+  min-height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.wrapper {
+  margin-top: 16px;
+}
+
+.title {
+  @extend %item;
+  background: $color-highlight;
+}
+
+.record {
+  @extend %item;
+  background: white;
+}
+
+::v-deep li.type-tabs-item {
+  background: #f5f5f5;
+  height: 52px;
+
+  &.selected {
+    background: $color-highlight;
+    color: white;
+
+    &::after {
+      display: none;
+    }
+  }
+}
+
+.comments {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
+
+</style>
